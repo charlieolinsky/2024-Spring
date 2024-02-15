10949b373adcacdd4e32837d191d9fe0ebcf3eda
charlieolinsky <charlesolinsky@gmail.com>
2024-02-15 15:19:13 -0500

    more npm, package.json, server, nodemon

--- server/index.js
+++ server/index.js
@@ -1,4 +1,22 @@
 /*
+    NodeJS using the CommonJS Module System, not ESModules
 */
+const express = require('express'); //this is an artifact of CommonJS
 
-console.log('Hello, world!')
+const app = express();
+const PORT = 3000;
+
+// Here we are setting up routes: the essence of a web server
+
+//If we have a get request on the root path, send back hello world
+app.get('/', (req, res) => {
+  res.send('Hello New Paltz!');
+});
+
+//this allows the app to conitnue listening for requests
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+});
+
+//Note, after any changes, you need to restart the server
+//Or you can use nodemon to automatically restart the server
