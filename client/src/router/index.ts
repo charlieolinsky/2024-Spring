99477c43cbd81da7be620438ba430404fa8e4b4f
charlieolinsky <charlesolinsky@gmail.com>
2024-03-18 13:29:15 -0400

    Created Login Badge

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,9 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { routes } from 'vue-router/auto-routes'
+import { refSession } from '../viewModel/session';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: routes
+})
+
+router.beforeEach((to, from, next) => {
+  const session = refSession();
+  //Router Guard to prevent unauthenticated users from accessing the app
+  if (!['/login', '/register'].includes(to.path) && !session.user) {
+    next('/login');
+    return;
+  }
+  console.log('beforeEach', from, to);
+  next();
 })
 
 export default router
