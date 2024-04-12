a3f330ceb3a095be2b831323d9dd6e52784572da
charlieolinsky <charlesolinsky@gmail.com>
2024-04-12 14:29:08 -0400

    post, patch, delete, and jsDoc

--- server/index.js
+++ server/index.js
@@ -1,24 +1,21 @@
-/*
-    NodeJS using the CommonJS Module System, not ESModules
-*/
-const express = require('express'); //this is an artifact of CommonJS
-const users = require('./controllers/users')
+const express = require("express");
+const users = require("./controllers/users");
+/* B"H
+ */
 
 const app = express();
 const PORT = 3000;
 
-// Here we are setting up routes: the essence of a web server
+app.use(express.json());
 
-//If we have a get request on the root path, send back hello world
-app.get('/', (req, res) => {
-  res.send('Hello New Paltz!');
-})
-.use('/api/v1/users', users)
+app
+  .get("/", (req, res) => {
+    res.send("Hello New Paltz!");
+  })
+  .use("/api/v1/users", users);
 
-//this allows the app to conitnue listening for requests
+// Error handling
+
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
 });
-
-//Note, after any changes, you need to restart the server
-//Or you can use nodemon to automatically restart the server
--- server/models/users.js
+++ server/models/users.js
@@ -1,20 +1,88 @@
-const data = require('../data/users.json');
+/* B"H
+ */
+const data = require("../data/users.json");
 
+/**
+ * @typedef {import('../../client/src/model/users').User} User
+ * */
+
+/**
+ * @returns {User[]}
+ * */
 function getAll() {
-    return data.items;
+  return data.items.map((x) => ({
+    ...x,
+    password: undefined,
+    bank: undefined,
+    ssn: undefined,
+  }));
 }
 
+/**
+ * @param {integer} id
+ * @returns {User}
+ * */
 function get(id) {
-    return data.items.find(item => item.id == id);
+  return data.items.find((item) => item.id == id);
 }
 
+/**
+ * @param {string} q
+ * @returns {User[]}
+ * */
 function search(q) {
-    return data.items.filter(item => 
-        new RegExp(q, 'i').test(item.firstName) ||
-        new RegExp(q, 'i').test(item.lastName) ||
-        new RegExp(q, 'i').test(item.email) );
+  return getAll().filter(
+    (item) =>
+      new RegExp(q, "i").test(item.firstName) ||
+      new RegExp(q, "i").test(item.lastName) ||
+      new RegExp(q, "i").test(item.email)
+  );
+}
+
+/**
+ * @param {User} user
+ * @returns {User}
+ * */
+function add(user) {
+  user.id = data.items.length + 1;
+  data.items.push(user);
+  return user;
+}
+
+/**
+ * @param {User} user
+ * @returns {User}
+ * */
+function update(user) {
+  const index = data.items.findIndex((item) => item.id == user.id);
+  if (index >= 0) {
+    data.items[index] = {
+      ...data.items[index],
+      ...user,
+    };
+    return user;
+  }
+  return null;
+}
+
+/**
+ * @param {integer} id
+ * @returns {boolean}
+ * */
+function remove(id) {
+  const index = data.items.findIndex((item) => item.id == id);
+  if (index >= 0) {
+    data.items.splice(index, 1);
+    return true;
+  }
+  return false;
 }
 
 module.exports = {
-    getAll, get, search
-}
+  getAll,
+  get,
+  search,
+  add,
+  update,
+  remove,
+};
