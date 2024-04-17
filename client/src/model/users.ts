a023ef6fcb1d1a7a79763c6bd9c62f4fc388f9c5
charlieolinsky <charlesolinsky@gmail.com>
2024-04-17 10:49:46 -0400

    created typed api function

--- client/src/model/users.ts
+++ client/src/model/users.ts
@@ -1,4 +1,4 @@
-import { rest } from "./myFetch";
+import { api } from "./myFetch";
 
 export interface User {
   id: number;
@@ -25,7 +25,7 @@ export interface Address {
   state: string;
 }
 
-export async function getUsers(): Promise<User[]> {
-  const data = await rest("http://localhost:3000/api/v1/users");
+export async function getUsers() {
+  const data = await api<User[]>("users");
   return data.data;
 }
